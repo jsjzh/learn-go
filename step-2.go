84210712c2c0ecc7acab0c761c90212b37ec9f51
jinzhehao <jinzhehao@souche.com>
2022-07-19 16:33:19 +0800

    feat: struct 结构体增加示例代码

--- step-2.go
+++ step-2.go
@@ -2,7 +2,63 @@ package main
 
 import "fmt"
 
+type company struct {
+	companyName string
+	companyAddr string
+}
+
+type Person struct {
+	name, work string
+	age        int
+	gender     int8
+
+	company
+
+	father *Person
+	mother *Person
+}
+
+func (person Person) FmtProfile() {
+	fmt.Printf("名字：%s\n", person.name)
+	fmt.Printf("年龄：%d\n", person.age)
+	fmt.Printf("性别：%d\n", person.gender)
+	fmt.Printf("工作：%s\n", person.work)
+	fmt.Printf("公司：%s\n", person.company.companyName)
+	fmt.Printf("公司：%s\n", person.companyName)
+}
+
+func (person *Person) addAge() {
+	person.age += 1
+}
+
 func main() {
-	count := 18
-	fmt.Printf("hello %d", count)
+
+	self := Person{
+		name:   "king",
+		age:    18,
+		gender: 1,
+		work:   "developer",
+		company: company{
+			companyName: "大搜车",
+		},
+	}
+
+	self.FmtProfile()
+	self.addAge()
+	self.FmtProfile()
+
+	self1 := Person{}
+	self2 := new(Person)
+	self3 := &Person{}
+
+	fmt.Printf("self1's type: %T\n", self1)
+	fmt.Printf("self1's name: %s\n", self1.name)
+	fmt.Printf("self2's type: %T\n", self2)
+	fmt.Printf("self2's name: %s\n", (*self2).name)
+	fmt.Printf("self3's type: %T\n", self3)
+	fmt.Printf("self3's name: %s\n", (*self3).name)
+
+	obj := map[string]int{"age": 18}
+
+	fmt.Printf("obj: %+v", obj)
 }
