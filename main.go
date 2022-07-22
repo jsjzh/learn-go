b24e72fc775495e8f0ae4ed667fb60047af85218
金哲豪 <jinzhehao@souche.com>
2022-07-22 09:38:37 +0800

    feat: 增加 gin 相关代码

--- main.go
+++ main.go
@@ -1,13 +1,48 @@
 package main
 
 import (
-	// _ "learn-go/review"
+	"log"
+	"net"
 
-	_ "learn-go/step_four"
-	// _ "learn-go/step_one"
-	// _ "learn-go/step_three"
-	// _ "learn-go/step_two"
+	"github.com/gin-gonic/gin"
 )
 
+// _ "learn-go/review"
+
+// _ "learn-go/step_four"
+// _ "learn-go/step_one"
+// _ "learn-go/step_three"
+// _ "learn-go/step_two"
+
+// middleWares
+// logger
+// controllers
+// routes
+// services
+// models
+// utils
+// crons
+// conf
+
 func main() {
+	router := gin.Default()
+
+	routerGroup := router.Group("/v1/api")
+
+	routerGroup.GET("/hello", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"status": "success",
+			"data":   routerGroup.BasePath(),
+		})
+	})
+
+	addrs, error := net.InterfaceAddrs()
+
+	if error != nil {
+		log.Println("error", error)
+	}
+
+	log.Println("addrs", addrs)
+
+	router.Run("127.0.0.1:7001")
 }
